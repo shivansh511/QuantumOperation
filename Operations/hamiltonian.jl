a122d795f7ced2736894f0343476bb611e6cdc44
Shivansh <58079966+shivansh511@users.noreply.github.com>
2025-08-11 22:58:17 +0100

    Add files via upload

---
+++ Operations/hamiltonian.jl
@@ -0,0 +1,28 @@
+using LinearAlgebra
+
+
+X=Matrix{ComplexF64}([0 1; 1 0])
+Y=Matrix{ComplexF64}([0 -1im; 1im 0])
+Z=Matrix{ComplexF64}([1 0; 0 -1])
+I=Matrix{ComplexF64}([1 0; 0 1])
+
+function TFI(N::Int, dim::Int=2)
+
+    H=zeros(ComplexF64, dim^N, dim^N)
+    matrix_list=fill(I, N)
+    matrix_list[1]=Z
+    matrix_list[2]=Z
+
+    println(matrix_list)
+    matrix_list=circshift(matrix_list,1)
+    println(matrix_list)
+    #=
+    for i 1:N-1
+        H += foldl(kron, matrix_list)
+        matrix_list = circshift(matrix_list, 1)
+    end
+    =#
+
+end
+
+TFI(4)
